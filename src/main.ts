dcd1a6a0451fbad21b63fd487b5f017b7ea98513
wildfirecode <wildfirecode13@gmail.com>
2018-10-28 19:28:46 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,41 @@
+import { loadImage } from "./assetUtil";
+
 class Main extends egret.DisplayObjectContainer {
     constructor() {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
     }
 
-    onAddToStage(event) {
-        let textfield = new egret.TextField();
-        this.addChild(textfield);
-        textfield.textColor = 0xff0000;
-        textfield.text = 'hello world';
+    onAddToStage() {
+        this.createGame();
+    }
+
+    async createGame() {
+        const assets = [
+            'assets/background.png',
+            'assets/hero.png',
+            'assets/enemy.png',
+        ];
+        const bitmaps = await loadImage(assets) as egret.Bitmap[];
+        const [bg, hero, enemy] = bitmaps;
+        this.addChild(bg);
+        this.addChild(hero);
+        this.addChild(enemy);
+
+        //设置飞机的锚点为飞机中心点
+        this.centerAnchor(hero);
+        this.centerAnchor(enemy);
+
+        enemy.x = this.stage.stageWidth / 2;
+        enemy.y = 200;
+
+        hero.x = this.stage.stageWidth / 2;
+        hero.y = this.stage.stageHeight - 100;
+    }
+
+    centerAnchor(displayObject: egret.DisplayObject) {
+        displayObject.anchorOffsetX = displayObject.width / 2;
+        displayObject.anchorOffsetY = displayObject.height / 2;
     }
 }
 
